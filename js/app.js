0b41b34d4f8693d8d1695eb517da48656a34a43c
Stijn Laurent <46028254+laurentstijn@users.noreply.github.com>
2025-04-29 11:54:44 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+import { init as initFirebase } from './firebase.js';
+import { init as initHarborManager } from './harborManager.js';
+import { init as initUI } from './uiManager.js';
+import { init as initDataManager } from './dataManager.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Initialisatie volgorde is belangrijk
+    initFirebase();
+    initHarborManager();
+    initUI();
+    initDataManager();
+    
+    console.log('Applicatie geïnitialiseerd');
+});
